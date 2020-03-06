52cee4f28f957d068db986eee793ad386b18734a
about7code <sanaullahhaifz@gmail.com>
2020-03-06 11:25:17 +0400

    Updates

---
+++ precache-manifest.d5ae081ad50dbe6d0e0fb6d0e45b3a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e80e66d55c27863d600206b930f191",
+    "url": "/travel-expo/index.html"
+  },
+  {
+    "revision": "ed7345a09ec5f56d6f68",
+    "url": "/travel-expo/static/css/main.6e9e3f3e.chunk.css"
+  },
+  {
+    "revision": "c4e91a764cc71803566a",
+    "url": "/travel-expo/static/js/2.5366868c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/travel-expo/static/js/2.5366868c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7345a09ec5f56d6f68",
+    "url": "/travel-expo/static/js/main.3eecaca3.chunk.js"
+  },
+  {
+    "revision": "a0ed02a9d25023065d39",
+    "url": "/travel-expo/static/js/runtime-main.78904958.js"
+  }
+]);
